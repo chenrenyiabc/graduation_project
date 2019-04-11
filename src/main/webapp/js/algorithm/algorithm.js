5dc30a9dcfb9fa366e82a278f7d7980cc438d5c5
chenry <chenry@akulaku.com>
2019-04-11 11:21:40 +0800

    dd

--- src/main/webapp/js/algorithm/algorithm.js
+++ src/main/webapp/js/algorithm/algorithm.js
@@ -1,6 +1,5 @@
 $(function () {
     var method = "query_algorithm";
-    console.log("dddd");
     $.ajax({
         url : 'AlgorithmServlet',
         type : 'post',
@@ -11,17 +10,19 @@ $(function () {
         success : function(data){
             for(var row in data){
                 console.log("row---" + data[row]);
-                $("#tbale_body").append("<tr></tr>");
+                //$("#tbale_body").append("<tr></tr>");
                 //for(var col in data[row]){
                 //    console.log("col---" + col);
-                $("#tbale_body tr:last").append("<td>"+ data[row]["id"] +"</td>");
-                $("#tbale_body tr:last").append("<td class='changemodify' id='name' value='"+data[row]["id"]+"'>"+ data[row]["algorithm_name"] +"</td>");
-                $("#tbale_body tr:last").append("<td class='changemodify' id='path' value='"+data[row]["id"]+"'>"+ data[row]["algorithm_path"] +"</td>");
-                $("#tbale_body tr:last").append("<td>" +
-                    "<div class='operate'><button type='button' id='modify' class='btn btn-warning' value='"+data[row]["id"]+"'>修改</button></div>" +
-                    "<div class='operate'><button type='button' id='delete' class='btn btn-danger' value='"+data[row]["id"]+"'>删除</button></div>" +
-                    "</td>");
-                //}
+                $("#tbale_body").append("<tr>" +
+                    "<td>"+ data[row]["id"] +"</td>" +
+                    "<td>"+ data[row]["algorithm_name"] +"</td>" +
+                    "<td>"+ data[row]["algorithm_path"] +"</td>" +
+                    "<td>" +
+                        "<button type='button' id='modify' class='btn btn-warning' value='"+data[row]["id"]+"'>修改</button>" +
+                        "<button type='button' id='delete' class='btn btn-danger' value='"+data[row]["id"]+"'>删除</button>" +
+                    "</td>" +
+                    "</tr>"
+                );
 
             }
         }
@@ -29,18 +30,7 @@ $(function () {
 
     $("#tbale_body").on("click","#modify",function(){
         var value = $(this).val();
-        console.log("value---" + value);
-        console.log("name_value---" + $(this).find("#name").html());
-        console.log("path_value---" + $(this).find("#path").html());
 
-        if($("#name").attr("value") == value){
-            $("#name").html("");
-            $("#name").html("<input type='text' class='form-control' placeholder='请输入新的算法名' aria-describedby='basic-addon1'>");
-        }
-        if($("#path").attr("value") == value) {
-            $("#path").html("");
-            $("#path").html("<input type='text' class='form-control' placeholder='请输入新的算法路径' aria-describedby='basic-addon1'>");
-        }
 
     })
 
